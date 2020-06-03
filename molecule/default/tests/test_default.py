cb13ac874e9d2a64911e477ddc6332e79c556e08
dbax <dan-aksenov@yandex.ru>
2020-06-03 17:13:11 +0300

    passing ansible variable to testinfra!

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,10 +1,16 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
+
+
+@pytest.fixture(scope='module')
+def AnsibleRoleDefaults(host):
+    return host.ansible('include_vars', '../../defaults/main.yml')['ansible_facts']
 
 
 def test_hosts_file(host):
@@ -14,8 +20,12 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
-# def test_etcd_service(host):
-#    etcd = host.service('postgresql*')
-#
-#    assert etcd.is_running
-#    assert etcd.is_enabled
+
+def test_etcd_service(host, AnsibleRoleDefaults):
+
+    postgresql_version = AnsibleRoleDefaults['postgresql_version']
+    
+    postgresql_service = host.service('postgresql-' + str(postgresql_version))
+
+    assert postgresql_service.is_running
+    assert postgresql_service.is_enabled
